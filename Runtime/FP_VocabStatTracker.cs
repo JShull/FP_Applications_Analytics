c3964fdd514651ec2a272f68cc688389f3f00c2c
John Shull <the.john.shull@gmail.com>
2025-05-18 13:33:31 -0400

    Setup of Repository

---
+++ Runtime/FP_VocabStatTracker.cs
@@ -0,0 +1,174 @@
+namespace FuzzPhyte.Applications.Analytics
+{
+    using FuzzPhyte.Utility.Analytics;
+    using System.Collections.Generic;
+    using FuzzPhyte.Utility.EDU;
+    using FuzzPhyte.Utility;
+    using FuzzPhyte.XR;
+    using UnityEngine;
+    using System.Linq;
+    public class FP_VocabStatTracker : MonoBehaviour
+    {
+        public static FP_VocabStatTracker Instance { get; private set; }
+       
+        
+        //we really just want to keep tabs on the number of times the audio/label was presented to start
+        protected Dictionary<FP_Vocab, FP_StatReporter_Int> vocabMediaInteracted = new();
+        //we also want to keep tabs on number of pick ups of this type of vocab
+        protected Dictionary<FP_Vocab,FP_StatReporter_Int> vocabInteracted = new();
+        [Space]
+        [Header("Details")]
+        public List<FPSpawner> TrackedSpawners = new List<FPSpawner>();
+        
+        protected List<FP_Vocab> allVocab = new List<FP_Vocab>();
+        [Tooltip("This object needs a stat reporter on it")]
+        public GameObject InteractionStatReporterPrefab;
+        public GameObject MediaStatReporterPrefab;
+        //public Dictionary<FP_Vocab,FP_StatReporter_Int> VocabTrackers { get => vocabTrackers; }
+        //public Dictionary<FP_Vocab,FP_StatReporter_Int> VocabInteractions { get => vocabInteractions; }
+        //public IReadOnlyDictionary<FP_Vocab, FP_StatReporter_Int> ReturnTrackers() => vocabMediaInteracted;
+        protected void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+                return;
+            }
+            Instance = this;
+        }
+        #region Setup
+        protected void Start()
+        {
+            //remove duplicates
+            allVocab = allVocab.Distinct().ToList();
+            //register for listeners now that we already have in our list
+        }
+#if UNITY_EDITOR
+        [Tooltip("Use this for testing")]
+        public List<FPWorldItem> TestWorldItems = new List<FPWorldItem>();
+        [ContextMenu("Setup FP World Item Test List Listeners")]
+        public void TestSetupFPItems()
+        {
+            foreach (FPWorldItem v in TestWorldItems)
+            {
+                RegisterItem(v);
+            }
+        }
+#endif
+        protected void OnEnable()
+        {
+            foreach(var spawner in TrackedSpawners)
+            {
+                if(spawner is FPXRSpawnStack)
+                {
+                    var stackSpawn = (FPXRSpawnStack)spawner;
+                    stackSpawn.OnSpawnedItem += HandleSpawnedItem;
+                }
+                else
+                {
+                    if(spawner is FPXRSpawnPieces)
+                    {
+                        var pieceSpawn = (FPXRSpawnPieces)spawner;
+                        pieceSpawn.OnSpawnedItem += HandleSpawnedItem;
+                    }
+                }
+            }
+        }
+
+        protected void OnDisable()
+        {
+            foreach (var spawner in TrackedSpawners)
+            {
+                if (spawner is FPXRSpawnStack)
+                {
+                    var stackSpawn = (FPXRSpawnStack)spawner;
+                    stackSpawn.OnSpawnedItem -= HandleSpawnedItem;
+                }
+                else
+                {
+                    if (spawner is FPXRSpawnPieces)
+                    {
+                        var pieceSpawn = (FPXRSpawnPieces)spawner;
+                        pieceSpawn.OnSpawnedItem -= HandleSpawnedItem;
+                    }
+                }
+            }
+        }
+        #endregion
+        protected void HandleSpawnedItem(GameObject obj)
+        {
+            var item = obj.GetComponent<FPWorldItem>();
+            if (item != null)
+            {
+                RegisterItem(item);
+            }
+        }
+
+        public void RegisterItem(FPWorldItem item)
+        {
+            item.ItemGrabbed += (fpItem, hand) => LogVocabInteraction(fpItem, InteractionStatReporterPrefab,vocabInteracted);
+            item.ItemRaySelect += (fpItem, hand) => LogVocabInteraction(fpItem, InteractionStatReporterPrefab,vocabInteracted);
+            item.ItemLabelActivated += (fpItem) => LogVocabInteraction(fpItem, MediaStatReporterPrefab,vocabMediaInteracted);
+        }
+
+        protected virtual void LogVocabInteraction(FPWorldItem item, GameObject prefab,Dictionary<FP_Vocab, FP_StatReporter_Int> storage)
+        {
+            var vocab = item.DetailedLabelData.VocabData;
+           
+            if(vocab == null)
+            {
+                return;
+            }
+            //build a list of all vocab including support vocab because it's still vocab
+            List<FP_Vocab>cachedVocab = new List<FP_Vocab>();
+            cachedVocab.Add(vocab);
+            var supportVocab = item.DetailedLabelData.SupportVocabData;
+            if (supportVocab != null)
+            {
+                //seen this support vocab?
+                for (int i = 0; i < supportVocab.Count; i++)
+                {
+                    cachedVocab.Add(supportVocab[i].SupportData);
+                }
+            }
+            //seen this vocab?
+            for(int i = 0; i < cachedVocab.Count; i++)
+            {
+                var aVocabCached = cachedVocab[i];
+                if (!allVocab.Contains(aVocabCached))
+                {
+                    allVocab.Add(aVocabCached);
+                }
+                if (!storage.TryGetValue(aVocabCached, out var reporter))
+                {
+                    reporter = CreateTrackerForVocab(prefab,aVocabCached);
+                    if(reporter != null)
+                    {
+                        storage[aVocabCached] = reporter;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                   
+                }
+                bool success = false;
+                reporter.NewStatData($"Interacted with {aVocabCached.Word}", ref success, 1);
+            }
+        }
+        protected virtual FP_StatReporter_Int CreateTrackerForVocab(GameObject statPrefab,FP_Vocab vocab)
+        {
+            var trackerGO = Instantiate(statPrefab, this.transform);
+            trackerGO.transform.localPosition = Vector3.zero;
+            trackerGO.name = $"StatReporter_{vocab.Word}";
+            var reporter = trackerGO.GetComponent<FP_StatReporter_Int>();
+            if (reporter == null)
+            {
+                Debug.LogError($"Missing a Stat reporter on the gameobject your spawning!");
+                Destroy(trackerGO);
+                return null;
+            }
+            return reporter;
+        }
+    }
+}
